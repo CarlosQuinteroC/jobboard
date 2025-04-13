6e448bcb3eeec64fcc61e114c10a86b43e71f015
Carlos Quintero <chqc11@gmail.com>
2025-04-12 23:17:17 -0400

    create Interest model and migration for tracking job interests

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,7 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
+
     /**
      * The attributes that are mass assignable.
      *
@@ -32,7 +33,11 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
-
+// Many-to-Many: A user can be interested in many jobs.
+    public function interestedJobs()
+    {
+        return $this->belongsToMany(Job::class, 'interests')->withTimestamps();
+    }
     /**
      * Get the attributes that should be cast.
      *
