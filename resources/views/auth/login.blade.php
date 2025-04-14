3b3f527436d9bc5eabc1969171135acb6628b728
Carlos Quintero <chqc11@gmail.com>
2025-04-13 21:52:02 -0400

    Updated the styling for the login, register and log out views

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,7 +26,11 @@
         </x-form-field>
 
         <div class="mt-6 flex items-center justify-end gap-x-6">
-            <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-            <x-form-button>Log in</x-form-button>
-        </div>    </form>
+            <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
+            <button type="submit"
+                    class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded leading-5 hover:text-gray-400 hover:bg-black focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-white active:text-red-700 transition ease-in-out duration-150 dark:bg-white dark:border-gray-600 dark:active:bg-gray-700 dark:focus:border-blue-800">
+                login
+            </button>
+        </div>
+    </form>
 @endsection
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -33,7 +33,11 @@
                         @auth
                             <form method="POST" action="/logout">
                                 @csrf
-                                <x-form-button>Log out</x-form-button>
+                   
+                                <button type="submit"
+                                        class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded leading-5 hover:text-gray-400 hover:bg-black focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-white active:text-red-700 transition ease-in-out duration-150 dark:bg-white dark:border-gray-600 dark:active:bg-gray-700 dark:focus:border-blue-800">
+                                    log out
+                                </button>
                             </form>
                         @endauth
                     </div>
