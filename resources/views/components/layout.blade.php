52fd6cca52bdf55874f9b02cc3a4c228c2fc16ee
Carlos Quintero <chqc11@gmail.com>
2025-04-13 23:08:52 -0400

    Added the loggin and logout buttons to the mobile, removed the home button and replaced itwith the icon link to home

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -44,6 +44,20 @@
                 </div>
                 <div class="-mr-2 flex md:hidden">
                     <!-- Mobile menu button -->
+                    @guest
+                        <x-nav-link href="/login" :active="request()->is('login')">Log in</x-nav-link>
+                        <x-nav-link href="/register" :active="request()->is('register')">Register</x-nav-link>
+                    @endguest
+                    @auth
+                        <form method="POST" action="/logout">
+                            @csrf
+
+                            <button type="submit"
+                                    class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded leading-5 hover:text-gray-400 hover:bg-black focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-white active:text-red-700 transition ease-in-out duration-150 dark:bg-white dark:border-gray-600 dark:active:bg-gray-700 dark:focus:border-blue-800">
+                                log out
+                            </button>
+                        </form>
+                    @endauth
                     <button type="button"
                             class="relative inline-flex items-center justify-center rounded-md bg-red-700 p-2 text-white hover:bg-black hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                             aria-controls="mobile-menu"
